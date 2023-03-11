881e3405fa291789d03e25fd985a823e24c1dd06
Saurabh Singh <108970897+Saurabhxcodes@users.noreply.github.com>
2023-03-11 14:27:22 +0530

    base

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import  ReactDOM  from 'react-dom';
-import App from './App';
-import "./index.css";
+// import App from './App';
+// import "./index.css";
+import Card from "./Cards";
+import "./Carde.css";
+import Sdata from "./Sdata";
+
+
+function ncard(val){
+    return(
+        <Card
+        imgsrc ={val.imgsrc}
+        title = {val.title}
+        sname = {val.sname}
+        link = {val.links}
+        />
+    );
+    
+} 
 
 ReactDOM.render(
-    <App/>,
+    <>
+    <h1 className='heading.style'> List of top 6 Netflix series 2023</h1>
+    {Sdata.map(ncard)}
+    </>,
     document.getElementById("root"));
+    
